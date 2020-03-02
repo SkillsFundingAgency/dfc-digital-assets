0215b0ac2cf58aee2ae8b5e8e70b1e41d77a13e3
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-03-02 10:40:24 +0000

    Refactor

--- src/nationalcareers_toolkit/assets/src/frontend/js/matchskills.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/matchskills.js
@@ -1,6 +1,8 @@
+var allChecked=false;
 $('#selectSkillsGovukLinkSkillSelectToggle').click(function(e) {
+    allChecked = !allChecked;
     e.preventDefault();
-    $("input:checkbox").prop('checked', function(i, current) { return !current; });
+    $("input:checkbox").prop('checked', allChecked);
     var selectAll = 'Select all';
     $(this).text($(this).text().includes(selectAll) ? 'Clear all' : selectAll);
 });
