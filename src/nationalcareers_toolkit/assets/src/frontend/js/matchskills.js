c9a06459f8f46a2279389c721963b874f5b411a1
GulshanBaryah <52538305+GulshanBaryah@users.noreply.github.com>
2020-03-03 09:16:44 +0000

    Update matchskills.js

--- src/nationalcareers_toolkit/assets/src/frontend/js/matchskills.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/matchskills.js
@@ -1,6 +1,8 @@
 $('#selectSkillsGovukLinkSkillSelectToggle').click(function(e) {
+    
     e.preventDefault();
     $("input:checkbox").prop('checked', function(i, current) { return !current; });
     var selectAll = 'Select all';
     $(this).text($(this).text().includes(selectAll) ? 'Clear all' : selectAll);
+
 });
