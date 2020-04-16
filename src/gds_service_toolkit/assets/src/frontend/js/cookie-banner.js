118d64818d8ebb511caebf47ec3e516b9821d0f1
gurney <Gurney@as-tech.co.uk>
2020-04-16 09:30:03 +0100

    Added JS for cookie - prefrences and banner

--- src/gds_service_toolkit/assets/src/frontend/js/cookie-banner.js
+++ src/gds_service_toolkit/assets/src/frontend/js/cookie-banner.js
@@ -1,62 +1,23 @@
-/*
-  Cookie methods
-  ==============
-  Usage:
-    Setting a cookie:
-    CookieBanner.init('hobnob', 'tasty', { days: 30 });
-    Reading a cookie:
-    CookieBanner.init('hobnob');
-    Deleting a cookie:
-    CookieBanner.init('hobnob', null);
-*/
-var CookieBanner = {
-  init: function (name, value, options) {
-    if (typeof value !== 'undefined') {
-      if (value === false || value === null) {
-        return CookieBanner.setCookie(name, '', { days: -1 })
-      } else {
-        return CookieBanner.setCookie(name, value, options)
-      }
-    } else {
-      return CookieBanner.getCookie(name)
-    }
-  },
-  setCookie: function (name, value, options) {
-    if (typeof options === 'undefined') {
-      options = {}
-    }
-    var cookieString = name + '=' + value + '; path=/'
-    if (options.days) {
-      var date = new Date()
-      date.setTime(date.getTime() + (options.days * 24 * 60 * 60 * 1000))
-      cookieString = cookieString + '; expires=' + date.toGMTString()
-    }
-    if (document.location.protocol === 'https:') {
-      cookieString = cookieString + '; Secure'
-    }
-    document.cookie = cookieString
-  },
-  getCookie: function (name) {
-    var nameEQ = name + '='
-    var cookies = document.cookie.split(';')
-    for (var i = 0, len = cookies.length; i < len; i++) {
-      var cookie = cookies[i]
-      while (cookie.charAt(0) === ' ') {
-        cookie = cookie.substring(1, cookie.length)
-      }
-      if (cookie.indexOf(nameEQ) === 0) {
-        return decodeURIComponent(cookie.substring(nameEQ.length))
-      }
-    }
-    return null
-  },
-  addCookieMessage: function () {
-    var message = document.querySelector('.js-cookie-banner')
-    var hasCookieMessage = (message && CookieBanner.init('seen_cookie_message') === null)
+$(document).ready(function () {
 
-    if (hasCookieMessage) {
-      message.style.display = 'block'
-      CookieBanner.init('seen_cookie_message', 'yes', { days: 28 })
+    if (typeof window.GOVUK === 'undefined') document.body.className = document.body.className.replace('js-enabled', '');
+
+    if (GOVUK.cookie("cookies_preferences_set")) {
+        $("#global-cookie-message").hide();
     }
-  }
-}
+    else {
+        //set defaults
+        GOVUK.setConsentCookie();
+    }
+
+    $("#accept-all-cookies").click(function () {
+
+        GOVUK.approveAllCookieTypes();
+        $("#confirmatiom-message").show();
+        window.GOVUK.cookie('cookies_preferences_set', 'true', { days: 365 })
+    });
+
+    $("#hide-cookies-message").click(function () {
+        $("#global-cookie-message").hide();
+    });
+});
