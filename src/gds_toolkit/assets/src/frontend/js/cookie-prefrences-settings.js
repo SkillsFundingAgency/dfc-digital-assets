ac4996cb733b116a54e44faac5b0c9444ac1e090
Gurnaik-Randhawa <Gurnaik-Randhawa@users.noreply.github.com>
2020-04-17 09:29:29 +0100

    Added js for setting page (#107)
    Co-authored-by: gurney <Gurney@as-tech.co.uk>

---
+++ src/gds_toolkit/assets/src/frontend/js/cookie-prefrences-settings.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    $("#global-cookie-message").hide();
+
+    if (!GOVUK.cookie("cookies_preferences_set")) {
+        GOVUK.setConsentCookie();
+    }
+
+    formInitalise();
+
+    $("#form-cookie-settings").submit(function (t) {
+        for (var e = t.target.getElementsByTagName("input"), n = {}, o = 0; o < e.length; o++) {
+            var i = e[o];
+            if (i.checked) {
+                var r = i.name.replace("cookies-", ""),
+                    a = "on" === i.value;
+                n[r] = a
+            }
+        }
+        window.GOVUK.setConsentCookie(n)
+        window.GOVUK.setCookie("cookies_preferences_set", !0, { days: 365 });
+        $("#cookie-settings-confirmation").show();
+        window.scrollTo(0, 0);
+        return false;
+    });
+});
+
+
+function formInitalise() {
+    var t = window.GOVUK.cookie("cookies_policy"),
+        e = JSON.parse(t);
+    for (var n in delete e.essential, e) {
+        (e[n] ? document.querySelector("input[name=cookies-" + n + "][value=on]") : document.querySelector("input[name=cookies-" + n + "][value=off]")).checked = !0
+    }
+}
