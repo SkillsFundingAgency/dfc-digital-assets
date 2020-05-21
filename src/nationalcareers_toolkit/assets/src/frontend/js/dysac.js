d1ef24768da6f73bdf25ab563332af8a246aca9e
Sumair Ahmed <snipered@hotmail.com>
2020-05-21 10:10:26 +0100

    added print button script

--- src/nationalcareers_toolkit/assets/src/frontend/js/dysac.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/dysac.js
@@ -1,13 +1,18 @@
-analytics.init()
-breadcrumbs.init()
+analytics.init();
+breadcrumbs.init();
 
 if (helpers.isPage('app-page--results')) {
-  results.short()
+    results.short();
 }
 
 if (helpers.isPage('app-page--results-long')) {
-  if (document.body.clientWidth >= 768) {
-    results.cardHeight()
-  }
-  results.long()
+    if (document.body.clientWidth >= 768) {
+        results.cardHeight();
+    }
+    results.long();
 }
+
+$('#dysac-print-button').on('click', function () {
+    window.print();
+    return false;
+});
