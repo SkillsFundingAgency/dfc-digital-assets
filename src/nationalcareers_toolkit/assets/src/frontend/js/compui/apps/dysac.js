0f7709473ac4562aa575eeb426315a553122c3b7
Sumair Ahmed <snipered@hotmail.com>
2020-06-10 17:13:43 +0100

    removed non working code

--- src/nationalcareers_toolkit/assets/src/frontend/js/compui/apps/dysac.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/compui/apps/dysac.js
@@ -12,14 +12,7 @@
 //    results.long();
 //}
 
-$('#dysac-validation-summary').ready(function () {
-    var form = $('#dysac-form');
-    form.validate();
-    var dysacValid = form.valid();
-    if (!dysacValid) {
-        compUiShell.validation.ShowErrorInPageTitle('dysac-validation-summary');
-    }
-});
+
 
 $(document).ready(function () {
     $('#dysac-print-button').on('click', function () {
