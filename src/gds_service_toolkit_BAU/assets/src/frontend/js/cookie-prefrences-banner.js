70c59b26183c7cd4f6e98f4a4461f71144875e8c
vallabha <vallabha24@gmail.com>
2020-07-30 17:10:35 +0100

    Removed script from national career service toolkit

--- src/gds_service_toolkit_BAU/assets/src/frontend/js/cookie-prefrences-banner.js
+++ src/gds_service_toolkit_BAU/assets/src/frontend/js/cookie-prefrences-banner.js
@@ -1,3 +1,6 @@
+//Set default state based on ehlstateCookie
+var ehlstateCookie = "ehlstateCookie";
+
 $(document).ready(function () {
 
     if (typeof cookiePrefrences === 'undefined') document.body.className = document.body.className.replace('js-enabled', '')
@@ -21,6 +24,43 @@ $(document).ready(function () {
             })
         }
     }
-})
+
+    //Exam help lin cookie banner functionality
+    var ehlCookie = CookieBanner.getCookie(ehlstateCookie);
+    if (ehlCookie === null) {
+
+        //Set cookie
+        CookieBanner.setCookie(ehlstateCookie, true, 28);
+        $("#ehl-hide-link").text("Hide message");
+        $('.ncs-toggle').show();
+    }
+    else if (ehlCookie === "true") {
+        //Set cookie
+        CookieBanner.setCookie(ehlstateCookie, true, 28);
+        $("#ehl-hide-link").text("Hide message");
+        $('.ncs-toggle').show();
+    }
+    else if (ehlCookie === "false") {
+        //Set cookie
+        CookieBanner.setCookie(ehlstateCookie, false, 28);
+        $("#ehl-hide-link").text("Show message");
+        $('.ncs-toggle').hide();
+    }
+
+    //On click functionality
+    $("#ehl-hide-link").click(function () {
+        $(".ncs-toggle").slideToggle();
+        if ($(this).text() === "Show message") {
+            $(this).text("Hide message");
+
+            // Update cookie status
+            CookieBanner.setCookie(ehlstateCookie, true, 28);
+        } else {
+            $(this).text("Show message");
+            // Update cookie status
+            CookieBanner.setCookie(ehlstateCookie, false, 28);
+        }
+    });
+});
 
 
