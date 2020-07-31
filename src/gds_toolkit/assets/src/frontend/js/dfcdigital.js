918c350027670548d5205755501f6caeca627674
vallabha <vallabha24@gmail.com>
2020-07-31 11:33:11 +0100

    Fixed ehl banner issue for job profile pages (gds_toolkit)

--- src/gds_toolkit/assets/src/frontend/js/dfcdigital.js
+++ src/gds_toolkit/assets/src/frontend/js/dfcdigital.js
@@ -9,6 +9,8 @@ dfc.digital = {
         }
     }
 };
+//Set default state for ehl banner based on ehlstateCookie
+var ehlstateCookie = "ehlstateCookie";
 
 $(document).ready(function () {
     $(".js-search-focus").ready(function () { dfc.digital.addFocus(".js-search-focus"); }).focus(function () { dfc.digital.addFocus(this) }).blur(function () { dfc.digital.addFocus(this) });
@@ -128,5 +130,42 @@ $('.js-autocomplete').each(function () {
         }, 
         minLength: $(this).data('autocomplete-minlength')
     });
+});
+
+//Exam help lin cookie banner functionality
+var ehlCookie = CookieBanner.getCookie(ehlstateCookie);
+if (ehlCookie === null) {
+
+    //Set cookie
+    CookieBanner.setCookie(ehlstateCookie, true, 28);
+    $("#ehl-hide-link").text("Hide message");
+    $('.ncs-toggle').show();
+}
+else if (ehlCookie === "true") {
+    //Set cookie
+    CookieBanner.setCookie(ehlstateCookie, true, 28);
+    $("#ehl-hide-link").text("Hide message");
+    $('.ncs-toggle').show();
+}
+else if (ehlCookie === "false") {
+    //Set cookie
+    CookieBanner.setCookie(ehlstateCookie, false, 28);
+    $("#ehl-hide-link").text("Show message");
+    $('.ncs-toggle').hide();
+}
+
+//On click functionality
+$("#ehl-hide-link").click(function () {
+    $(".ncs-toggle").slideToggle();
+    if ($(this).text() === "Show message") {
+        $(this).text("Hide message");
+
+        // Update cookie status
+        CookieBanner.setCookie(ehlstateCookie, true, 28);
+    } else {
+        $(this).text("Show message");
+        // Update cookie status
+        CookieBanner.setCookie(ehlstateCookie, false, 28);
+    }
 });
 
