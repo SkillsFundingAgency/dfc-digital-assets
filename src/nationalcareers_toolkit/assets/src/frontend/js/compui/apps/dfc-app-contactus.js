590eaaf7a1f327ef6c76e6d011aceceb525b1323
Ian Crisp <49161034+IanCrisp@users.noreply.github.com>
2020-10-29 14:35:50 +0000

    Removed deprecated webchat code (#346)

--- src/nationalcareers_toolkit/assets/src/frontend/js/compui/apps/dfc-app-contactus.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/compui/apps/dfc-app-contactus.js
@@ -10,31 +10,9 @@ class DfcAppContactUs {
     }
 
     initialise() {
-        if (window.location.pathname.endsWith(this.pathForWebchat) || window.location.pathname.endsWith(this.pathForlocalWebchat)) {
-            this.initialiseWebchatView();
-        }
+
         if (window.location.pathname.endsWith(this.pathForEnterYourDetails) || window.location.pathname.endsWith(this.pathForlocalEnterYourDetails)) {
             this.initialisEnterYourDetailsView();
-        }
-    }
-
-    initialiseWebchatView() {
-        //Have to inject the iFrame as we cant have html in the <script> block
-        //<noscript> iFrame in the markup is use when no JS
-        var chatUrl = document.getElementById('ChatUrl');
-        if (chatUrl) {
-            var iFrameContainer = document.getElementById('chatcontainer');
-            if (iFrameContainer) {
-                var iFrame = document.createElement('iframe');
-                iFrame.setAttribute('id', 'webchatframe');
-                iFrame.src = chatUrl.value;                     // + '?GAID=' + CompUiUtilties.getCookie('_ga');
-                iFrame.classList = 'dfc-app-contact-us-Webchat';
-                iFrame.scrolling = 'no';
-                iFrame.title = 'webchat';
-
-                iFrameContainer.appendChild(iFrame);
-                iFrameContainer.style.cssText = 'width:100%; height: 1200px; -webkit-overflow-scrolling:touch';
-            }
         }
     }
 
