79715a733d40d0e03d496b801d9b8df4809fd187
Ian Crisp <icrisp@program-systems.co.uk>
2020-11-04 11:50:19 +0000

    Added webchat script

---
+++ src/gds_service_toolkit_BAU/assets/src/frontend/js/modules/dfc-app-webchat.js
@@ -0,0 +1,14 @@
+﻿DfcAppWebchat = {
+    initialise: function (webchatDomain) {
+        $('a[href^="/webchat/chat"]').click(function (e) {
+            if (typeof ChatRed != "undefined") {
+                let chatRed = new ChatRed('NCS-Live', webchatDomain);
+                chatRed.run();
+                if (typeof window.botmanWidget != "undefined") {
+                    e.preventDefault();
+                    window.botmanWidget.open();
+                }
+            }
+        });
+    }
+}
