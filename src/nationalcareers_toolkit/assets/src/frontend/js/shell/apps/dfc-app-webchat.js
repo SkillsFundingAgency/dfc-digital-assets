46d514239e788e1e63b7c73fb6be7ebc66426af6
Ian Crisp <icrisp@program-systems.co.uk>
2020-11-12 15:35:06 +0000

    Updated 'let' to use 'var' instead for IE11 compatibility

--- src/nationalcareers_toolkit/assets/src/frontend/js/shell/apps/dfc-app-webchat.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/shell/apps/dfc-app-webchat.js
@@ -5,7 +5,7 @@ DfcAppWebchat.prototype = {
     initialise: function (shellWebchatDomain) {
         $('a[href^="/webchat/chat"]').click(function (e) {
             if (typeof ChatRed != "undefined") {
-                let chatRed = new ChatRed('NCS-Live', shellWebchatDomain);
+                var chatRed = new ChatRed('NCS-Live', shellWebchatDomain);
                 chatRed.run();
                 if (typeof window.botmanWidget != "undefined") {
                     e.preventDefault();
