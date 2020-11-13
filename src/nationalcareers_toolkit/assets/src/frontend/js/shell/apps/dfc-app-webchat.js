fe0279447913a61fcc235a5b47353d3b505b8eb7
Ian Crisp <icrisp@program-systems.co.uk>
2020-11-13 16:35:03 +0000

    Updated webchat initialisation for Comp UI

--- src/nationalcareers_toolkit/assets/src/frontend/js/shell/apps/dfc-app-webchat.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/shell/apps/dfc-app-webchat.js
@@ -3,13 +3,13 @@ var DfcAppWebchat = function () {
 
 DfcAppWebchat.prototype = {
     initialise: function (shellWebchatDomain) {
+        var chatRed = new ChatRed('NCS-Live', shellWebchatDomain);
+        chatRed.run();
         $('a[href^="/webchat/chat"]').click(function (e) {
             if (typeof ChatRed != "undefined") {
-                var chatRed = new ChatRed('NCS-Live', shellWebchatDomain);
-                chatRed.run();
-                if (typeof window.botmanWidget != "undefined") {
+                if (typeof window.botmanChatWidget != "undefined") {
                     e.preventDefault();
-                    window.botmanWidget.open();
+                    window.botmanChatWidget.open();
                 }
             }
         });
