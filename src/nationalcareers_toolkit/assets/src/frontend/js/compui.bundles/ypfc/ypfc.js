3618834d7eb83b1ca4836a5d755c5ac5a6efae72
snipered2003 <snipered@hotmail.com>
2022-01-11 12:27:49 +0000

    added js (#618)
    * added js
    
    * added new line
    
    * fixed build

---
+++ src/nationalcareers_toolkit/assets/src/frontend/js/compui.bundles/ypfc/ypfc.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    $('#all-ypfc-button').on('click', function (e) {
+        $('.work').show();
+        $('.workandstudy').show();
+        $('.study').show();
+        e.preventDefault();
+        return false;
+    });
+
+    $('#combined-ypfc-button').on('click', function (e) {
+        $('.work').hide();
+        $('.workandstudy').show();
+        $('.study').hide();
+        e.preventDefault();
+        return false;
+    });
+
+    $('#study-ypfc-button').on('click', function (e) {
+        $('.work').hide();
+        $('.workandstudy').hide();
+        $('.study').show();
+        e.preventDefault();
+        return false;
+    });
+
+    $('#work-ypfc-button').on('click', function (e) {
+        $('.work').show();
+        $('.workandstudy').hide();
+        $('.study').hide();
+        e.preventDefault();
+        return false;
+    });
+});
