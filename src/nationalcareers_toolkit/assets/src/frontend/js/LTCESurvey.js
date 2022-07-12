ee2df22ff90ae05a7f65ed78da078656d645bcd8
SamDonnelly23 <91659794+SamDonnelly23@users.noreply.github.com>
2022-07-12 15:11:48 +0100

    Changed JS to allow for a fire once mechanism
    added local variable that is set on interaction so the survey only fires once

--- src/nationalcareers_toolkit/assets/src/frontend/js/LTCESurvey.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/LTCESurvey.js
@@ -1,9 +1,15 @@
 //This JavaScript is the functionality for the next page and exit page buttons on the LTCE popup surveys
+var survey = localStorage.getItem('Survey');
+if(survey){
+document.getElementById('popupsurvey').style.display = 'none'}
+else{document.getElementById('popupsurvey').style.display = 'block'};
 document.querySelectorAll("#exitbtn").forEach(item => item.addEventListener('click', function(){
         document.getElementById('popupsurvey').style.display = 'none'
+        localStorage.setItem('Survey',true);
     }));
 document.querySelectorAll("#btn").forEach(item => item.addEventListener('click', function(){
     this.parentElement.style.display = 'none';
     var nextpage = "survey" +this.getAttribute('name');
     document.getElementById(nextpage).style.display='inline';
+    localStorage.setItem('Survey',true);
     }));
