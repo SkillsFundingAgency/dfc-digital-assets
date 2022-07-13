fb958f3afd4ed381a476275caf504b640565d942
Sam Donnelly <sadon@netcompany.com>
2022-07-13 16:43:51 +0100

    Fixed JS issue, Else calling non existent element

--- src/nationalcareers_toolkit/assets/src/frontend/js/LTCESurvey.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/LTCESurvey.js
@@ -1,16 +1,13 @@
-//This JavaScript is the functionality for the next page and exit page buttons on the LTCE popup surveys
-var Surveyexist = document.getElementById('popupsurvey');
-if(typeof(Surveyexist) != 'undefined' && Surveyexist != null){
-if(localStorage.getItem('Survey')){
-document.getElementById('popupsurvey').style.display = 'none'}
-else{document.getElementById('popupsurvey').style.display = 'block'}};
-document.querySelectorAll("#exitbtn").forEach(item => item.addEventListener('click', function(){
-        document.getElementById('popupsurvey').style.display = 'none'
+if(localStorage.getItem('Survey' && document.getElementById('popupsurvey')!= null)){
+    document.getElementById('popupsurvey').style.display = 'none'}
+    else if (document.getElementById('popupsurvey')!= null){document.getElementById('popupsurvey').style.display = 'block'};
+    document.querySelectorAll("#exitbtn").forEach(item => item.addEventListener('click', function(){
+            document.getElementById('popupsurvey').style.display = 'none'
+            localStorage.setItem('Survey',true);
+        }));
+    document.querySelectorAll("#btn").forEach(item => item.addEventListener('click', function(){
+        this.parentElement.style.display = 'none';
+        var nextpage = "survey" +this.getAttribute('name');
+        document.getElementById(nextpage).style.display='inline';
         localStorage.setItem('Survey',true);
-    }));
-document.querySelectorAll("#btn").forEach(item => item.addEventListener('click', function(){
-    this.parentElement.style.display = 'none';
-    var nextpage = "survey" +this.getAttribute('name');
-    document.getElementById(nextpage).style.display='inline';
-    localStorage.setItem('Survey',true);
-    }));
+        }));
