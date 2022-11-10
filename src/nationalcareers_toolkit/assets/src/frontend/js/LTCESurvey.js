692fce439bb8b10fa550ee51db8bace7351516f6
Thomas Slatter <thomas.slatter@netcompany.com>
2022-11-10 14:56:29 +0000

    Moved survey varible from localStorage to sessionStorage

--- src/nationalcareers_toolkit/assets/src/frontend/js/LTCESurvey.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/LTCESurvey.js
@@ -1,35 +1,35 @@
-if ((localStorage.getItem('Survey') && document.getElementById('popupsurvey') != null)) {
+if ((sessionStorage.getItem('Survey') && document.getElementById('popupsurvey') != null)) {
     document.getElementById('popupsurvey').style.display = 'none'
 };
 //else if (document.getElementById('popupsurvey') != null) { document.getElementById('popupsurvey').style.display = 'flex' };
 
-if ((localStorage.getItem('Survey') && document.getElementsByClassName('job-profile-feedback').length > 0)) {
+if ((sessionStorage.getItem('Survey') && document.getElementsByClassName('job-profile-feedback').length > 0)) {
     document.getElementsByClassName('job-profile-feedback').forEach(item => item.style.display = 'block')
 };
 
 document.querySelectorAll("#popupsurvey #exitbtn").forEach(item => item.addEventListener('click', function () {
     document.getElementById('popupsurvey').style.display = 'none'
-    localStorage.setItem('Survey', true);
+    sessionStorage.setItem('Survey', true);
 }));
 document.querySelectorAll("#popupsurvey #btn").forEach(item => item.addEventListener('click', function () {
     this.parentElement.style.display = 'none';
     var nextpage = "survey" + this.getAttribute('name');
     document.getElementById(nextpage).style.display = 'inline';
-    localStorage.setItem('Survey', true);
+    sessionStorage.setItem('Survey', true);
 }));
 
-if ((localStorage.getItem('extendedSurvey') && document.getElementById('extendedsurvey') != null)) {
+if ((sessionStorage.getItem('extendedSurvey') && document.getElementById('extendedsurvey') != null)) {
     document.getElementById('extendedsurvey').style.display = 'none'
 }
 else if (document.getElementById('extendedsurvey') != null) { document.getElementById('extendedsurvey').style.display = 'block' };
 
 document.querySelectorAll("#extendedexitbtn").forEach(item => item.addEventListener('click', function () {
     document.getElementById('extendedsurvey').style.display = 'none'
-    localStorage.setItem('extendedSurvey', true);
+    sessionStorage.setItem('extendedSurvey', true);
 }));
 document.querySelectorAll("#extendedbtn").forEach(item => item.addEventListener('click', function () {
     this.parentElement.style.display = 'none';
     var nextpage = "survey" + this.getAttribute('name');
     document.getElementById(nextpage).style.display = 'inline';
-    localStorage.setItem('extendedSurvey', true);
+    sessionStorage.setItem('extendedSurvey', true);
 }));
