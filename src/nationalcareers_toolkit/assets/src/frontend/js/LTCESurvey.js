33606b870eea99f1880740ae961c6d6461341f54
Thomas Slatter <thomas.slatter@netcompany.com>
2022-11-14 11:53:16 +0000

    Added cookie for sitewide survey display lock - this is currently set to 5 mins, but we have the desired 28 days expiry set in line 3 (commented out for now)

--- src/nationalcareers_toolkit/assets/src/frontend/js/LTCESurvey.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/LTCESurvey.js
@@ -1,21 +1,31 @@
-if ((sessionStorage.getItem('Survey') && document.getElementById('popupsurvey') != null)) {
+function setSurveyCookie(isTrue = true) {
+    var date = new Date();
+    //date.setTime(date.getTime() + (28 * 24 * 60 * 60 * 1000)); // This ia for the actual expiry outside for PROD (28 days)
+    date.setTime(date.getTime() + (5 * 60 * 1000)); // This is for testing purposes (5mins)
+    console.log(date.toUTCString());
+    document.cookie = 'Survey=' + true + '; expires=' + date.toUTCString() + ';path=/'
+}
+
+console.log(CookieBanner.getCookie('Survey'));
+
+if ((CookieBanner.getCookie('Survey') && document.getElementById('popupsurvey') != null)) {
     document.getElementById('popupsurvey').style.display = 'none'
 };
-//else if (document.getElementById('popupsurvey') != null) { document.getElementById('popupsurvey').style.display = 'flex' };
 
-if ((sessionStorage.getItem('Survey') && document.getElementsByClassName('job-profile-feedback').length > 0)) {
+if ((CookieBanner.getCookie('Survey') && document.getElementsByClassName('job-profile-feedback').length > 0)) {
     document.getElementsByClassName('job-profile-feedback').forEach(item => item.style.display = 'block')
 };
 
 document.querySelectorAll("#popupsurvey #exitbtn").forEach(item => item.addEventListener('click', function () {
-    document.getElementById('popupsurvey').style.display = 'none'
-    sessionStorage.setItem('Survey', true);
+    document.getElementById('popupsurvey').style.display = 'none';
+    setSurveyCookie();
 }));
 document.querySelectorAll("#popupsurvey #btn").forEach(item => item.addEventListener('click', function () {
     this.parentElement.style.display = 'none';
     var nextpage = "survey" + this.getAttribute('name');
     document.getElementById(nextpage).style.display = 'inline';
-    sessionStorage.setItem('Survey', true);
+    setSurveyCookie();
+    //sessionStorage.setItem('Survey', true);
 }));
 
 if ((sessionStorage.getItem('extendedSurvey') && document.getElementById('extendedsurvey') != null)) {
