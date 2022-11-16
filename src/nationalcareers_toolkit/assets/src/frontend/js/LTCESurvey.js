f9bba0f11ed3496cc2771079b5b74c623ea4e6bb
tslatternet <114909672+tslatternet@users.noreply.github.com>
2022-11-16 14:08:11 +0000

    Feature/ltce 119 (#742)
    * Added survey styles to survey rating file and modified js selectors in ltce survey
    
    * Fixed selector reference on line 14
    
    * Added bottom: 0px to hopefully fix sticky not working
    
    * Added display: none; to the survey styles by default (this is to allow Google Optimize to work properly)
    
    * Commented out condition to display popup survey, as this interferes with Google Optimize scripts
    
    * Possible fix for js error in dev
    
    * Hopeful fix for the js error in browser
    
    * added padding to survey and max-width to survey-container class to ensure width sizing is uniform to the rest of the site content
    
    * Removed padding and added calculated width for possible styling fix
    
    * Moved survey varible from localStorage to sessionStorage
    
    * Added cookie for sitewide survey display lock - this is currently set to 5 mins, but we have the desired 28 days expiry set in line 3 (commented out for now)
    
    * Fixed typo in code commit
    
    * Removed commented lines and console.log commands (for tidier code)

--- src/nationalcareers_toolkit/assets/src/frontend/js/LTCESurvey.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/LTCESurvey.js
@@ -1,33 +1,42 @@
-if((localStorage.getItem('Survey') && document.getElementById('popupsurvey')!= null)){
-    document.getElementById('popupsurvey').style.display = 'none'}
-    else if (document.getElementById('popupsurvey')!= null){document.getElementById('popupsurvey').style.display = 'block'};
+function setSurveyCookie(isTrue = true) {
+    var date = new Date();
+    //date.setTime(date.getTime() + (28 * 24 * 60 * 60 * 1000)); // This is for the actual expiry outside for PROD (28 days)
+    date.setTime(date.getTime() + (5 * 60 * 1000)); // This is for testing purposes (5mins)
+    console.log(date.toUTCString());
+    document.cookie = 'Survey=' + true + '; expires=' + date.toUTCString() + ';path=/'
+}
 
-if((localStorage.getItem('Survey') && document.getElementsByClassName('job-profile-feedback')!= null)){
-    document.getElementsByClassName('job-profile-feedback').style.display = 'block'
+if ((CookieBanner.getCookie('Survey') && document.getElementById('popupsurvey') != null)) {
+    document.getElementById('popupsurvey').style.display = 'none'
 };
 
-    document.querySelectorAll("#exitbtn").forEach(item => item.addEventListener('click', function(){
-            document.getElementById('popupsurvey').style.display = 'none'
-            localStorage.setItem('Survey',true);
-        }));
-    document.querySelectorAll("#btn").forEach(item => item.addEventListener('click', function(){
-        this.parentElement.style.display = 'none';
-        var nextpage = "survey" +this.getAttribute('name');
-        document.getElementById(nextpage).style.display='inline';
-        localStorage.setItem('Survey',true);
-        }));
+if ((CookieBanner.getCookie('Survey') && document.getElementsByClassName('job-profile-feedback').length > 0)) {
+    document.getElementsByClassName('job-profile-feedback').forEach(item => item.style.display = 'block')
+};
 
-        if((localStorage.getItem('extendedSurvey') && document.getElementById('extendedsurvey')!= null)){
-            document.getElementById('extendedsurvey').style.display = 'none'}
-            else if (document.getElementById('extendedsurvey')!= null){document.getElementById('extendedsurvey').style.display = 'block'};
-        
-            document.querySelectorAll("#extendedexitbtn").forEach(item => item.addEventListener('click', function(){
-                    document.getElementById('extendedsurvey').style.display = 'none'
-                    localStorage.setItem('extendedSurvey',true);
-                }));
-            document.querySelectorAll("#extendedbtn").forEach(item => item.addEventListener('click', function(){
-                this.parentElement.style.display = 'none';
-                var nextpage = "survey" +this.getAttribute('name');
-                document.getElementById(nextpage).style.display='inline';
-                localStorage.setItem('extendedSurvey',true);
-                }));
+document.querySelectorAll("#popupsurvey #exitbtn").forEach(item => item.addEventListener('click', function () {
+    document.getElementById('popupsurvey').style.display = 'none';
+    setSurveyCookie();
+}));
+document.querySelectorAll("#popupsurvey #btn").forEach(item => item.addEventListener('click', function () {
+    this.parentElement.style.display = 'none';
+    var nextpage = "survey" + this.getAttribute('name');
+    document.getElementById(nextpage).style.display = 'inline';
+    setSurveyCookie();
+}));
+
+if ((sessionStorage.getItem('extendedSurvey') && document.getElementById('extendedsurvey') != null)) {
+    document.getElementById('extendedsurvey').style.display = 'none'
+}
+else if (document.getElementById('extendedsurvey') != null) { document.getElementById('extendedsurvey').style.display = 'block' };
+
+document.querySelectorAll("#extendedexitbtn").forEach(item => item.addEventListener('click', function () {
+    document.getElementById('extendedsurvey').style.display = 'none'
+    sessionStorage.setItem('extendedSurvey', true);
+}));
+document.querySelectorAll("#extendedbtn").forEach(item => item.addEventListener('click', function () {
+    this.parentElement.style.display = 'none';
+    var nextpage = "survey" + this.getAttribute('name');
+    document.getElementById(nextpage).style.display = 'inline';
+    sessionStorage.setItem('extendedSurvey', true);
+}));
