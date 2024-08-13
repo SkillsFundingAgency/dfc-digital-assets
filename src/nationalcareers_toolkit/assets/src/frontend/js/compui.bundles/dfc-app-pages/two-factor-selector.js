bff96bae685f54c401ef1d1d061a1218e4a05703
Siva MURUGANANDHAM <Siva.MURUGANANDHAM@EDUCATION.GOV.UK>
2024-08-14 00:38:05 +0100

    wip - validation

--- src/nationalcareers_toolkit/assets/src/frontend/js/compui.bundles/dfc-app-pages/two-factor-selector.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/compui.bundles/dfc-app-pages/two-factor-selector.js
@@ -3,10 +3,36 @@ $(document).ready(function () {
     var levelOne = $('#triageLevelOne');
     var levelTwo = $('#triageLevelTwo');
     var levelTwoItems = [];
+    hideTriageFromErrorMessage();
+    $('#triage-tool-submit-button').on("click", function (e) {
+        if (!$('#triageLevelOne').val()) {
+            showTriageFromErrorMessage();
+            e.preventDefault();
+        } else {
+            hideTriageFromErrorMessage();
+        }
+        
+    });
+
+    function showTriageFromErrorMessage() {
+        $('#level-one-error-group').addClass('govuk-form-group--error');
+        $('#triage-tool-validation-summary').removeClass('validation-summary-valid')
+        $('#level-one-error').removeClass('hidden-triage-form-error');
+        levelOne.addClass('govuk-select--error');
+
+    }
+
+    function hideTriageFromErrorMessage() {
+        $('#triage-tool-validation-summary').addClass('validation-summary-valid')
+        $('#level-one-error-group').removeClass('govuk-form-group--error');
+        $('#level-one-error').addClass('hidden-triage-form-error');
+        levelOne.removeClass('govuk-select--error');
+    }
 
     levelOne.change(function () {
         var selectedLevelOne = $('#triageLevelOne').val();
         if (selectedLevelOne) {
+            hideTriageFromErrorMessage();
             levelTwo.removeAttr('disabled');
             var apiCall = {
                 url: '/api/triageleveltwo',
@@ -38,6 +64,7 @@ $(document).ready(function () {
                 });
             }
         } else {
+            showTriageFromErrorMessage();
             var optionHtml = updateOptionList(levelTwoItems, selectedLevelOne);
             levelTwo.html(optionHtml);
             levelTwo.attr('disabled', 'disabled');
