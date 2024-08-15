a9b954ce4c6bfc976c7adbf2808fac7ee078016c
Siva MURUGANANDHAM <Siva.MURUGANANDHAM@EDUCATION.GOV.UK>
2024-08-15 11:33:12 +0100

    fixed review comments

--- src/nationalcareers_toolkit/assets/src/frontend/js/compui.bundles/dfc-app-pages/two-factor-selector.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/compui.bundles/dfc-app-pages/two-factor-selector.js
@@ -1,38 +1,33 @@
 $(document).ready(function () {
-
     var levelOne = $('#triageLevelOne');
     var levelTwo = $('#triageLevelTwo');
     var levelTwoItems = [];
-    hideTriageFromErrorMessage();
+    hideTriageFormErrorMessage();
     $('#triage-tool-submit-button').on("click", function (e) {
         if (!$('#triageLevelOne').val()) {
-            showTriageFromErrorMessage();
+            showTriageFormErrorMessage();
             e.preventDefault();
         } else {
-            hideTriageFromErrorMessage();
+            hideTriageFormErrorMessage();
         }
-        
     });
-
-    function showTriageFromErrorMessage() {
+    function showTriageFormErrorMessage() {
         $('#level-one-error-group').addClass('govuk-form-group--error');
         $('#triage-tool-validation-summary').removeClass('validation-summary-valid')
         $('#level-one-error').removeClass('hidden-triage-form-error');
         levelOne.addClass('govuk-select--error');
 
     }
-
-    function hideTriageFromErrorMessage() {
+    function hideTriageFormErrorMessage() {
         $('#triage-tool-validation-summary').addClass('validation-summary-valid')
         $('#level-one-error-group').removeClass('govuk-form-group--error');
         $('#level-one-error').addClass('hidden-triage-form-error');
         levelOne.removeClass('govuk-select--error');
     }
-
     levelOne.change(function () {
         var selectedLevelOne = $('#triageLevelOne').val();
         if (selectedLevelOne) {
-            hideTriageFromErrorMessage();
+            hideTriageFormErrorMessage();
             levelTwo.removeAttr('disabled');
             var apiCall = {
                 url: '/api/triageleveltwo',
@@ -53,43 +48,35 @@ $(document).ready(function () {
                         }
                         var optionHtml = updateOptionList(levelTwoItems, selectedLevelOne);
                         levelTwo.html(optionHtml);
-
                     },
                     failure: function () {
                         console.log('Failure, Retrieving Triage level two options');
                     },
                     error: function () {
-                        console.log('Error, Retrieving Triage Tool two options');
+                        console.log('Error, Retrieving Triage level two options');
                     }
                 });
             }
         } else {
-            showTriageFromErrorMessage();
+            showTriageFormErrorMessage();
             var optionHtml = updateOptionList(levelTwoItems, selectedLevelOne);
             levelTwo.html(optionHtml);
             levelTwo.attr('disabled', 'disabled');
 
         }
     });
-
     function updateOptionList(levelTwoItems, selectedLevelOne) {
         var optionHtml = '';
-
         $.each(levelTwoItems,
             function (index, levelTwo) {
                 if (levelTwo && levelTwo.levelOne && selectedLevelOne === levelTwo.levelOne.title) {
-
-                    optionHtml = optionHtml + generateOptionHtml(levelTwo.title, levelTwo.title);
+                    optionHtml += generateOptionHtml(levelTwo.title, levelTwo.title);
                 }
             });
         return optionHtml;
     }
     function generateOptionHtml(value, item)
     {
-            return '<option value="' + value + '">' + item + '</option>';
+        return '<option value="' + value + '">' + item + '</option>';
     }
-       
-
-      
-    
 });
