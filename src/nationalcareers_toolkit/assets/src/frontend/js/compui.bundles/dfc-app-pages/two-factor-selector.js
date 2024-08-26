ede19a20ba850ba57260db5ca543ab0952c5ff5f
Siva MURUGANANDHAM <Siva.MURUGANANDHAM@EDUCATION.GOV.UK>
2024-08-26 22:36:07 +0100

    JS validation changes

--- src/nationalcareers_toolkit/assets/src/frontend/js/compui.bundles/dfc-app-pages/two-factor-selector.js
+++ src/nationalcareers_toolkit/assets/src/frontend/js/compui.bundles/dfc-app-pages/two-factor-selector.js
@@ -1,33 +1,77 @@
 $(document).ready(function () {
     var levelOne = $('#triageLevelOne');
     var levelTwo = $('#triageLevelTwo');
+    var education = $('#education').val();
     var levelOneItems = [];
     var levelTwoItems = [];
     hideTriageFormErrorMessage();
     $('#triage-tool-submit-button').on("click", function (e) {
         if (!$('#triageLevelOne').val()) {
+            hideTriageFormLevelTwoErrorMessage();
             showTriageFormErrorMessage();
             e.preventDefault();
         } else {
             hideTriageFormErrorMessage();
+            if (!$('#triageLevelTwo').val()) {
+                showTriageFormLevelTwoErrorMessage();
+                e.preventDefault();
+                return;
+            } else {
+                hideTriageFormLevelTwoErrorMessage();
+            }
+            
+
         }
     });
     function showTriageFormErrorMessage() {
         $('#level-one-error-group').addClass('govuk-form-group--error');
         $('#triage-tool-validation-summary').removeClass('validation-summary-valid')
         $('#level-one-error').removeClass('hidden-triage-form-error');
+        $('#level-one-summary-error').removeClass('hidden-triage-form-error');
         levelOne.addClass('govuk-select--error');
+        
     }
     function hideTriageFormErrorMessage() {
         $('#triage-tool-validation-summary').addClass('validation-summary-valid')
         $('#level-one-error-group').removeClass('govuk-form-group--error');
         $('#level-one-error').addClass('hidden-triage-form-error');
+        $('#level-one-summary-error').addClass('hidden-triage-form-error');
         levelOne.removeClass('govuk-select--error');
+    }
+    function showTriageFormLevelTwoErrorMessage() {
+       // var errorGroup = $('#triageLevelOne').attr("error-group-id");
+        // $('span[error-group="' + errorGroup + '"]').removeClass('hidden-triage-form-error');
+        $('#level-one-error-group').addClass('govuk-form-group--error');
+        $('#triage-tool-validation-summary').removeClass('validation-summary-valid')
+        if (levelOne.val() === education) {
+            $('#edu-level-two-summary-error').removeClass('hidden-triage-form-error');
+            $('#edu-level-two-error').removeClass('hidden-triage-form-error');
+            $('#level-two-summary-error').addClass('hidden-triage-form-error');
+            $('#level-two-error').addClass('hidden-triage-form-error');
+        } else {
+            $('#edu-level-two-summary-error').addClass('hidden-triage-form-error');
+            $('#edu-level-two-error').addClass('hidden-triage-form-error');
+            $('#level-two-summary-error').removeClass('hidden-triage-form-error');
+            $('#level-two-error').removeClass('hidden-triage-form-error');
+        }
+        levelTwo.addClass('govuk-select--error');
+    }
+    function hideTriageFormLevelTwoErrorMessage() {
+        $('#triage-tool-validation-summary').addClass('validation-summary-valid')
+        $('#level-one-error-group').removeClass('govuk-form-group--error');
+       // if (levelOne.val() === education) {
+            $('#edu-level-two-summary-error').addClass('hidden-triage-form-error');
+            $('#edu-level-two-error').addClass('hidden-triage-form-error');
+       // }
+        //else {
+            $('#level-two-summary-error').addClass('hidden-triage-form-error');
+            $('#level-two-error').addClass('hidden-triage-form-error');
+        //}
+        levelTwo.removeClass('govuk-select--error');
     }
     levelOne.change(function () {
         var selectedLevelOne = $('#triageLevelOne').val();
         if (selectedLevelOne) {
-            hideTriageFormErrorMessage();
             levelTwo.removeAttr('disabled');
             var apiCall = {
                 url: '/api/Ajax/Action',
@@ -75,7 +119,10 @@ $(document).ready(function () {
                 });
             }
         } else {
-            showTriageFormErrorMessage();
+            var l1 = levelOneItems.find(x => x.value === selectedLevelOne)
+            if (l1 && l1.levelTwo) {
+                levelTwoItems = l1.levelTwo.contentItems;
+            }
             var optionHtml = updateOptionList(levelTwoItems, selectedLevelOne);
             levelTwo.html(optionHtml);
             levelTwo.attr('disabled', 'disabled');
@@ -85,7 +132,7 @@ $(document).ready(function () {
         var optionHtml = '';
         $.each(levelTwoItems,
             function (index, levelTwo) {
-                    optionHtml += generateOptionHtml(levelTwo.title, levelTwo.title);
+                optionHtml += generateOptionHtml(levelTwo.value, levelTwo.title);
             });
         return optionHtml;
     }
